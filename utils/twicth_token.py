7fb7cd58845c6a2843dad200ccbc5594a65bb811
João Saldanha <jlucassaldanha@gmail.com>
2025-09-08 20:51:13 -0300

    Add: Refresh token function

---
+++ utils/twicth_token.py
@@ -0,0 +1,36 @@
+import httpx
+from fastapi import Depends, Request, HTTPException
+from sqlalchemy.orm import Session
+
+from main import CLIENT_ID, CLIENT_SECRET
+from models import User
+from .dependecies import get_session
+
+async def refresh_twitch_token(user_id: int, session: Session = Depends(get_session)):
+	user = session.query(User).filter(User.id==user_id).first()
+
+	if not user:
+		HTTPException(status_code=404, detail="Usuário não encontrado")
+
+	async with httpx.AsyncClient() as client:
+		response = await client.post(
+			"https://id.twitch.tv/oauth2/token",
+			params={
+				"grant_type": "refresh_token",
+				"refresh_token": user.refresh_token,
+				"client_id": CLIENT_ID,
+				"client_secret": CLIENT_SECRET
+			}
+		)
+
+	if response.status_code != 200:
+		raise HTTPException(status_code=401, detail="Falha ao usar refresh token")
+	
+	tokens = response.json()
+	user.access_token = tokens["access_token"]
+	user.refresh_token = tokens["refresh_token"]
+	user.expires_in = tokens["expires_in"]
+
+	session.commit()
+
+	return user
