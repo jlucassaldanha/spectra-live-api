5e8723310bdade208c4f15642364a72492178c7b
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-09-09 13:41:34 -0300

    Update models.py
    Adiciona tabela de usuarios fora de vista

--- models.py
+++ models.py
@@ -25,3 +25,20 @@ class User(Base):
         self.display_name = display_name
         self.email = email
         self.profile_image_url = profile_image_url
+
+class UnviewUsers(Base):
+    __tablename__ = "unview_users"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    user_id = Column("user_id", ForeignKey("user.id"))
+    twitch_id = Column("twitch_id", Integer)
+    login = Column("login", String)
+    display_name = Column("display_name", String)
+    profile_image_url = Column("profile_image_url", String)
+
+    def __init__(self, user_id: int, twitch_id: int, login: str, display_name: str, profile_image_url: str):
+        self.user_id = user_id
+        self.twitch_id = twitch_id
+        self.login = login
+        self.display_name = display_name
+        self.profile_image_url = profile_image_url
