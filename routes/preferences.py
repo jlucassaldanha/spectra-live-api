85002201c04982cfd444c5810b9e326e9240c961
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-09-10 10:14:01 -0300

    Update preferences.py

--- routes/preferences.py
+++ routes/preferences.py
@@ -4,11 +4,15 @@ from fastapi.responses import RedirectResponse, JSONResponse
 import httpx, uuid
 
 from main import CLIENT_ID, CLIENT_SECRET
-from utils import get_session, create_jwt, decode_jwt, get_current_user, refresh_twitch_token
+from utils import get_session, create_jwt, decode_jwt, get_current_user, refresh_twitch_token, twitch_get_endpoint
 
 from sqlalchemy.orm import Session
-from models import User
+from models import User, TwitchUsers
 
 preferences_router = APIRouter(prefix="/preferences", tags=["Preferences"])
 
 # Lista moderadores para poder colocar na lista de fora de vista
+preferences_router.get("/unview")
+async def get_moderators(current_user: User = Depends(get_current_user), session: Session = Depends(get_session)):
+	
+	pass
