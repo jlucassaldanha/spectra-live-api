66d2b84836a8e9d61ed045e5106602f2c14caeee
João Saldanha <jlucassaldanha@gmail.com>
2025-09-20 18:01:21 -0300

    Update
    Retirando o uso da tabela twitch users nas rotas

--- routes/preferences.py
+++ routes/preferences.py
@@ -1,47 +1,38 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 
 from utils import get_session, get_current_user, twitch_get_endpoint
 
 from sqlalchemy.orm import Session
-from models import User, TwitchUsers, UnviewUsers
+from models import User, UnviewUsers
 from schemas import UserIdSchema
 
 preferences_router = APIRouter(prefix="/preferences", tags=["Preferences"])
 
 @preferences_router.post("/add/unview")
 async def set_unview(twitch_ids: UserIdSchema, current_user: User = Depends(get_current_user), session: Session = Depends(get_session)):	
-	users = session.query(TwitchUsers).filter(TwitchUsers.twitch_id.in_(twitch_ids.twitch_ids)).all()
-
-	found = {user.twitch_id for user in users}
-	missing = set(twitch_ids.twitch_ids) - found
-
-	if len(missing) > 0:
+	
+	twitch_users_ids = []
+	for i in range(0, len(twitch_ids.twitch_ids), 100):
+		ids = twitch_ids.twitch_ids[i:i + 100]
+		
 		response = await twitch_get_endpoint(
 			current_user=current_user,
 			session=session,
 			endpoint="https://api.twitch.tv/helix/users",
-			params={"id":missing}
+			params={"id":ids}
 		)
 
-		for data in response["data"]:
-			new_twitch_user = TwitchUsers(
-				data["id"],
-				data["login"],
-				data["display_name"],
-				data["profile_image_url"]
-			)
-			session.add(new_twitch_user)
+		data = response["data"]
+		twitch_users_ids += [user["id"] for user in data]
+		
 
-		session.commit()
+	for id in twitch_users_ids:
+		unview = session.query(UnviewUsers).filter(UnviewUsers.channel_id==current_user.twitch_id).filter(UnviewUsers.twitch_user_id==id).first()
 
-		users = session.query(TwitchUsers).filter(TwitchUsers.twitch_id.in_(twitch_ids.twitch_ids)).all()
-
-	for user in users:
-		unview = session.query(UnviewUsers).filter(UnviewUsers.channel_id==current_user.twitch_id).filter(UnviewUsers.twitch_user_id==user.twitch_id).first()
 		if not unview:
 			new_unview = UnviewUsers(
 				channel_id=current_user.twitch_id,
-				twitch_user_id=user.twitch_id
+				twitch_user_id=id
 			)
 			session.add(new_unview)
 
@@ -51,10 +42,9 @@ async def set_unview(twitch_ids: UserIdSchema, current_user: User = Depends(get_
 
 @preferences_router.delete("/remove/unview")
 async def remove_unview(twitch_ids: UserIdSchema, current_user: User = Depends(get_current_user), session: Session = Depends(get_session)):	
-	users = session.query(TwitchUsers).filter(TwitchUsers.twitch_id.in_(twitch_ids.twitch_ids)).all()
 
-	for user in users:
-		unview = session.query(UnviewUsers).filter(UnviewUsers.channel_id==current_user.twitch_id).filter(UnviewUsers.twitch_user_id==user.twitch_id).first()
+	for id in twitch_ids.twitch_ids:
+		unview = session.query(UnviewUsers).filter(UnviewUsers.channel_id==current_user.twitch_id).filter(UnviewUsers.twitch_user_id==id).first()
 		if unview:
 			session.delete(unview)
 
